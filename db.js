431c7aad246fad40309b0ea5f752cbc2ab9a80dd
JP <jua.ahumadaa@duocuc.cl>
2024-07-14 03:43:36 -0400

    JP

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 import {createPool} from "mysql2/promise.js";
 
 export const pool = createPool({
-    host: "127.0.0.1",
+    host: "viaduct.proxy.rlwy.net",
     user: "root",
-    password: "",
-    database: "apiblog",
-    port: 3306
+    password: "EDmRQxmtUDpaeYSYzGWQdbMcaazZmmZL",
+    database: "railway",
+    port: 41405
 });
--- index.js
+++ index.js
@@ -11,21 +11,21 @@ app.use(morganMiddleware);
 app.use(corsMiddleware);
 
 app.get('/blog', async (req, res) => {
-    const result = await pool.query("SELECT * FROM apiblogapp_post");
+    const result = await pool.query("SELECT * FROM Post");
     res.json(result[0]);
 });
 
 app.get('/blog/:id', async (req, res) => {
-    const result = await pool.query("SELECT * FROM apiblogapp_post WHERE id = ?", [req.params.id]);
+    const result = await pool.query("SELECT * FROM Post WHERE id = ?", [req.params.id]);
     res.json(result[0]);
 });
 
 app.get('/comentario/:id', async (req, res) => {
-    const result = await pool.query("SELECT * FROM apiblogapp_coments WHERE post_id = ?", [req.params.id]);
+    const result = await pool.query("SELECT * FROM Coments WHERE post_id = ?", [req.params.id]);
     res.json(result[0]);
 });
 
 
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+  console.log('Server is running on port 3000');
 });
